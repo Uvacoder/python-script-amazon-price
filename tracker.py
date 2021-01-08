0e07b5123a24a6e29a9192793413ccaa82781d28
Nigel Davis <ndavis10@uoguelph.ca>
2021-01-08 16:37:24 -0500

    added reading and writing to files

--- tracker.py
+++ tracker.py
@@ -3,22 +3,24 @@
 import requests
 import os
 import pandas as pd
+import math
 
 from bs4 import BeautifulSoup
+from csv import writer
 from glob import glob
 from time import sleep
 from dotenv import load_dotenv
 from datetime import datetime
 
 HEADERS = ({'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.61 Safari/537.36',
-            'Accept-Language': 'en-US,en;q=0.5'})
+            'Accept-Language': 'en-US,en;q=0.5'
+})
+DOMAIN = 'https://www.amazon.ca'
 
-def login() -> requests.Session:
+
+def login(session: requests.Session) -> None:
 
     loginSite = 'https://www.amazon.ca/gp/sign-in.html'
-
-    session = requests.Session()
-    session.headers = HEADERS
 
     soup = BeautifulSoup(session.get(loginSite).content, 'lxml')
 
@@ -44,30 +46,93 @@ def login() -> requests.Session:
 
     return session
 
-load_dotenv()
 
-session = login()
+def addNewItems(session: requests.Session, tracker: pd.DataFrame) -> None:
 
-page = session.get(os.getenv('WISHLIST_URL'), headers=HEADERS)
-soup = BeautifulSoup(page.content, 'lxml')
+    # check for new items in the wishlist
 
-title = soup.find(id='profile-list-name').get_text().strip()
-print(title)
+    page = session.get(os.getenv('WISHLIST_URL'), headers=HEADERS)
+    soup = BeautifulSoup(page.content, 'lxml')
 
-item_list = soup.find(id='wl-item-view')
-items = item_list.find_all('li')
+    title = soup.find(id='profile-list-name').get_text().strip()
+    print(title)
 
-for item in items:   
-    url = 'https://www.amazon.ca' + item.find('h3', class_='a-size-base').find('a')['href'] 
-    subpage = session.get(url, headers=HEADERS)
-    subsoup = BeautifulSoup(subpage.content, 'lxml')
+    item_list = soup.find(id='wl-item-view')
+    items = item_list.find_all('li')
 
+    for item in items:
+
+        # Check to see if the item is already in the csv file
+
+        id = item['data-itemid']
+        if id in list(tracker.id):
+            continue
+
+        name = item.find('h3', class_='a-size-base').find('a')['title'] 
+        url = DOMAIN + item.find('h3', class_='a-size-base').find('a')['href']
+        
+        buy_below = item.find(id='itemCommentRow_'+id).text.strip()
+
+        # Add Item
+
+        with open('products.csv', 'a', newline='', encoding="utf-8") as file:
+            writer(file).writerow([id, name, url, buy_below])
+
+
+def getPrice(soup):
     try:
-        price = subsoup.find(id='priceblock_ourprice').get_text().strip()
+        price = soup.find(id='priceblock_ourprice').get_text().strip()
     except:
         try:
             price = soup.find(id='priceblock_saleprice').get_text().strip()
         except:
-            price = ''
+            try:
+                price = soup.find(id='priceblock_dealprice').get_text().strip()
+            except:
+                price = ''
+    return price
 
-    print(price)
+
+def getItemsInfo(session: requests.Session, tracker: pd.DataFrame) -> None:
+    trackerURLs = tracker.url
+
+    for x, url in enumerate(list(trackerURLs)):
+        page = session.get(url, headers=HEADERS)
+        soup = BeautifulSoup(page.content, 'lxml')
+
+        id = soup.find(id='sourceCustomerOrgListItemID')['value']
+        price = getPrice(soup)
+        title = soup.find(id='productTitle').get_text().strip()
+        
+        for x, Id in enumerate(list(tracker.id)):
+            if Id == id:
+                buy_below = list(tracker.buybelow)[x]
+
+        log = pd.DataFrame({
+            'date': datetime.now().strftime('%d/%m/%y %H:%M'),
+            'id': id,
+            'url': url,
+            'title': title,
+            'buy_below': buy_below,
+            'price': price
+        }, index=[x])
+
+    filename = 'history/checked_' + datetime.now().strftime('%d-%m-%y_%H-%M') + '.xlsx'
+    log.to_excel(filename)
+
+
+
+def main(): 
+    load_dotenv()
+    session = requests.Session()
+    session.headers = HEADERS
+    
+    login(session)
+
+    tracker = pd.read_csv('products.csv')
+
+    addNewItems(session, tracker)
+    getItemsInfo(session, tracker)
+
+
+main()
