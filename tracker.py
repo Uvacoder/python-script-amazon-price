42b83fd0a6282b0b0e1e80e0d33999c91d36ca81
Nigel Davis <ngeldvis@gmail.com>
2021-05-08 16:40:21 -0400

    overhall: updated email design and web scrapping implementation (now using selenium)

--- tracker.py
+++ tracker.py
@@ -1,212 +1,267 @@
 # Tracks the prices of items in an Amazon Wishlist
 
-import requests
 import os
-import smtplib
-
+import time
 import pandas as pd
 
-from csv import writer
-from bs4 import BeautifulSoup
 from time import sleep
 from dotenv import load_dotenv
-from datetime import datetime
-from playsound import playsound
+
+# Sending Emails
+import ssl
+import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-DOMAIN = 'https://www.amazon.ca'
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+# Selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+
+load_dotenv()
+
+FILENAME = os.getenv('FILENAME')
+
+
 
 class Item():
 
-    def __init__(self, name, price, id):
+    def __init__(self, name, price, id, want):
         self.name = name
         self.price = price
         self.id = id
+        self.want = want
 
-def login(session): # Unused in current version
 
-    print('Logging in...')
-
-    login_site = 'https://www.amazon.ca/gp/sign-in.html'
-
-    response = session.get(login_site)
-    soup = BeautifulSoup(response.content, 'lxml')
-
-    # get all inputs required for login
-
-    signin_data = {}
-    form = soup.find('form', {'name': 'signIn'})
-    for field in form.find_all('input'):
-        try:
-            signin_data[field['name']] = field['value']
-        except:
-            pass
+def updateItem(data, df):
     
-    signin_data[u'email'] = os.getenv('EMAIL')
-    signin_data[u'password'] = os.getenv('PASSWORD')
-    
-    # submit post request
-
-    post_response = session.post('https://www.amazon.ca/ap/signin', data=signin_data)
-    
-    # test if login was successful
-
-    post_soup = BeautifulSoup(post_response.content , 'lxml')
-
-    if post_soup.find_all('title')[0].text == 'Your Account':
-        print('Login Successful')
-    else:
-        print('Login Failed')
-        exit()
-
-def updateItem(data, tracker):
-    
-    ids = tracker.Id.tolist()
+    ids = df.Id.tolist()
     index = ids.index(data[0])
-    row = tracker.values[index].tolist()
-    newrow = [row[0], row[1], "{:.2f}".format(row[2])]
-    if newrow[2] == 'nan':
-        newrow[2] = ''
+    row = df.values[index].tolist()
 
-    if newrow != data:
-        with open('products.csv', 'r') as readfile:
-            lines = readfile.readlines()
-            for x, line in enumerate(lines):
-                with open('temp.csv', 'a', newline='', encoding="utf-8") as writefile:
-                    if x-1 == index:
-                        writer(writefile).writerow(data)
-                    else:
-                        writefile.write(line.strip())
-                        writefile.write('\n')
+    if row != data:
+        try:
+            df.at[index, 'Want'] = data[2]
+            df.to_csv(FILENAME, index=False);
+        except Exception as e:
+            print('Error: failed to update file')
+            print(e)
+
+
+def addNewItems(driver: WebDriver, df: pd.DataFrame) -> None:
+
+    items = driver.find_elements_by_css_selector('#wl-item-view li')
     
-        if os.path.exists('products.csv'):
-            os.remove('products.csv')
-            os.rename('temp.csv', 'products.csv')
-        else:
-            print('error deleting file')
-
-def addNewItems(session: requests.Session, tracker: pd.DataFrame) -> None:
-
-    page = session.get(os.getenv('WISHLIST_URL'), headers=HEADERS)
-    soup = BeautifulSoup(page.content, 'lxml')
-
-    item_list = soup.find(id='wl-item-view')
-    items = item_list.find_all('li')
-
     for item in items:
 
-        id = item['data-itemid']
-        name = item.find('h3', class_='a-size-base').find('a')['title']
+        item_id = item.get_attribute('data-itemid')
+        item_title = driver.find_element_by_id('itemName_'+item_id).get_attribute('title')
+        item_subtitle = driver.find_element_by_id('item-byline-'+item_id).text
+        item_name = item_title + ' ' + item_subtitle
         try:
-            buy_below = str(item.find(id='itemCommentRow_'+id).text.strip())
+            item_want = driver.find_element_by_id('itemComment_'+item_id).text.strip()
         except:
-            buy_below = ''
+            item_want = ''
         
-        # Check to see if the item is already in the csv file
-        data = [id, name, str(buy_below)]
-        if id in list(tracker.Id):
-            updateItem(data, tracker)
+        new_item = [item_id, item_name, item_want]
+       
+        if item_id in list(df.Id):
+            updateItem(new_item, df)
             continue      
 
-        with open('products.csv', 'a', newline='', encoding="utf-8") as file:
-            writer(file).writerow(data)
-
-def getPrice(soup):
-    try:
-        price = soup.find(id='priceblock_ourprice').get_text().strip()
-    except:
+        data = pd.DataFrame([new_item], columns=['Id', 'Name', 'Want'])
         try:
-            price = soup.find(id='priceblock_saleprice').get_text().strip()
-        except:
-            try:
-                price = soup.find(id='priceblock_dealprice').get_text().strip()
-            except:
-                return ''
-    return price[5::]
+            df = df.append(data, ignore_index=True, sort=False)
+            df.to_csv(FILENAME, index=False)
+        except Exception as e:
+            print('Error: failed to update file')
+            print(e)
 
-def send_email(item: Item) -> None:
 
-    receiver_email = os.getenv('EMAIL')
-    sender_email = os.getenv('DEV_EMAIL')
-    sender_password = os.getenv('DEV_PASSWORD')
+def send_email(items: list) -> None:
+
+    # getting email values
+
+    port = 465
+    smtpserver = 'smtp.gmail.com'
+
+    receiver_email = os.getenv('RECEIVER_EMAIL')
+    sender_email = os.getenv('SENDER_EMAIL')
+    sender_password = os.getenv('SENDER_PASSWORD')
+
+    # create email body
 
     msg = MIMEMultipart('alternative')
-    msg['Subject'] = 'Price Alert'
+    msg['Subject'] = 'Weekly Wishlist Price Alert'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
 
-    message = f'An item on your wishlist dropped below asking price!\n{item.name} is now ${item.price}!'
-    text = MIMEText(message, 'plain', 'utf-8')
-    msg.attach(text)
+    if len(items) > 0:
 
-    with smtplib.SMTP_SSL('64.233.184.108') as server:
+        html = '''
+            <p style="text-align: center;
+                color: #000000;">Some items on your wishlist are under your asking price!</p>
+            <table style="width: 50%;
+                    margin: 25px 0;
+                    border-collapse: collapse;
+                    border-radius: 5px 5px 0 0;
+                    overflow: hidden;
+                    margin: auto;">
+                <thead>
+                    <tr style="background-color: #fd983a;
+                            color: #ffffff;
+                            text-align: left;">
+                        <th style="padding: 12px 15px;
+                            text-align: left;">Item</th>
+                        <th style="padding: 12px 15px;
+                            text-align: left;">Price</th>
+                        <th style="padding: 12px 15px;
+                            text-align: left;">Wanted Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+            '''
+        for x, item in enumerate(items):
+            if x == len(items) - 1:
+                if x % 2 == 0:
+                    html += f'''
+                    <tr style="border-bottom: 1px solid #dddddd;
+                            border-bottom: 2px solid #fd983a;
+                            color: #000000;">
+                        <td style="padding: 12px 15px;
+                            text-align: left;">{item.name}</td>
+                        <td style="padding: 12px 15px;
+                            text-align: left;">${item.price:.2f}</td>
+                        <td style="padding: 12px 15px;
+                            text-align: left;">${item.want:.2f}</td>
+                    </tr>
+                    '''
+                else:
+                    html += f'''
+                    <tr style="border-bottom: 1px solid #dddddd; 
+                            background-color: #f3f3f3;
+                            border-bottom: 2px solid #fd983a;
+                            color: #000000;">
+                        <td style="padding: 12px 15px;
+                            text-align: left;">{item.name}</td>
+                        <td style="padding: 12px 15px;
+                            text-align: left;">${item.price:.2f}</td>
+                        <td style="padding: 12px 15px;
+                            text-align: left;">${item.want:.2f}</td>
+                    </tr>
+                    '''
+            else:
+                if x % 2 == 0:
+                    html += f'''
+                    <tr style="border-bottom: 1px solid #dddddd;
+                            color: #000000;">
+                        <td style="padding: 12px 15px;
+                            text-align: left;">{item.name}</td>
+                        <td style="padding: 12px 15px;
+                            text-align: left;">${item.price:.2f}</td>
+                        <td style="padding: 12px 15px;
+                            text-align: left;">${item.want:.2f}</td>
+                    </tr>
+                    '''
+                else:
+                    html += f'''
+                    <tr style="border-bottom: 1px solid #dddddd; 
+                            background-color: #f3f3f3;
+                            color: #000000;">
+                        <td style="padding: 12px 15px;
+                            text-align: left;">{item.name}</td>
+                        <td style="padding: 12px 15px;
+                            text-align: left;">${item.price:.2f}</td>
+                        <td style="padding: 12px 15px;
+                            text-align: left;">${item.want:.2f}</td>
+                    </tr>
+                    '''
+
+        html += '''
+                </tbody>
+            </table>
+            '''
+
+        part1 = MIMEText(html, 'html')
+        msg.attach(part1)
+
+    else:
+        text = 'None of the items on your wishlist are under your asking price.'
+        msg.attach(MIMEText(text, 'plain'))
+
+    # sending email
+
+    context = ssl.create_default_context()
+    
+    with smtplib.SMTP_SSL(smtpserver, port, context=context) as server:
         try:
             server.login(sender_email, sender_password)
-            server.sendmail(sender_email, receiver_email, msg.as_string().encode('ascii'))
+            server.sendmail(sender_email, receiver_email, msg.as_string())
             print('Email sent.')
         except Exception as e:
             print('Email failed to send.')
             print(e)
 
-def getPrices(session, tracker) -> None:
-    
-    page = session.get(os.getenv('WISHLIST_URL'), headers=HEADERS)
-    soup = BeautifulSoup(page.content, 'lxml')
 
-    item_list = soup.find(id='wl-item-view')
-    items = item_list.find_all('li')
+def getPrices(driver, df) -> None:
+
+    items = driver.find_elements_by_css_selector('#wl-item-view li')
+
+    found_items = []
 
     for item in items:
 
-        id = item['data-itemid']
+        item_id = item.get_attribute('data-itemid')
+        item_title = driver.find_element_by_id('itemName_'+item_id).get_attribute('title')
+        item_subtitle = driver.find_element_by_id('item-byline-'+item_id).text
+        item_name = item_title + ' ' + item_subtitle
 
-        rownum = tracker['Id'].tolist().index(id)
-        buy_below = str(tracker['Buy Below'].tolist()[rownum])
+        rownum = df['Id'].tolist().index(item_id)
+        want = df['Want'].tolist()[rownum]
 
-        try:
-            priceTXT = item.find('span', class_='a-price-whole').text + item.find('span', class_='a-price-fraction').text
+        try: 
+            priceA = driver.find_element_by_css_selector('#item_' + item_id + ' .a-price-whole').text
+            priceB = driver.find_element_by_css_selector('#item_' + item_id + ' .a-price-fraction').text
+            priceTXT = priceA + priceB
         except:
             continue
 
-        if buy_below != 'nan':
-            if float(priceTXT) <= float(buy_below):
-                print('Price Alert!')
-                playsound('rsc/notify.mp3')
+        if float(priceTXT) <= float(want):
+            new_item = Item(item_name, float(priceTXT), item_id, want)
+            found_items.append(new_item)
+    
+    send_email(found_items)
 
-                try:
-                    name = item.find('h3', class_='a-size-base').find('a')['title']
-                    newItem = Item(name, float(priceTXT), id)
-                    send_email(newItem)
-                except:
-                    pass
-        
+
+def load_site(url) -> WebDriver:
+
+    opts = webdriver.ChromeOptions()
+    opts.headless = True
+    driver = webdriver.Chrome(options=opts)
+    driver.get(url)
+    sleep(1)
+
+    elem = driver.find_element_by_tag_name('body')
+
+    pagedowns = 10
+    while pagedowns:
+        elem.send_keys(Keys.END)
+        time.sleep(0.2)
+        pagedowns -= 1
+
+    return driver
+
+
 def main(): 
-    
-    load_dotenv()
 
-    session = requests.Session()
-    session.headers = HEADERS
-    
-    # login(session)
-    # ^------------^ 
-    # unable to use if wishlist is private since too many login requests 
-    # were sent and the sign-in process requires re-captcha challenges
+    # check to see if any new items have been added
+    df = pd.read_csv(FILENAME)
+    driver = load_site(os.getenv('WISHLIST_URL'))
+    addNewItems(driver, df)
 
-    intervals = 1
-    interval_length = 60 # seconds
+    # check the prices and if any have dropped below buy_below
+    getPrices(driver, df) 
 
-    interval = 0
-    while interval < intervals:
-
-        # check to see if any new items have been added
-        tracker = pd.read_csv('products.csv')
-        addNewItems(session, tracker)
-
-        # check the prices and if any have dropped below buy_below
-        tracker = pd.read_csv('products.csv')
-        getPrices(session, tracker)
-
-        # sleep(interval_length)
-        interval += 1 # update interval
 
 main()
